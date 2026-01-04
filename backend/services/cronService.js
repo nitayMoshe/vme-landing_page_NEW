4a6eda476c944dfbf4ceb9a56e522c4f3dcffffe
nitay <nitaymoshe1234@email.com>
2026-01-04 11:29:31 +0200

    commented all mongo related code

--- backend/services/cronService.js
+++ backend/services/cronService.js
@@ -1,45 +1,45 @@
-import cron from "node-cron";
-import mongoose from "mongoose";
-import User from "../schemas/User.js"; // your Mongoose model
-import dotenv from "dotenv";
+// import cron from "node-cron";
+// import mongoose from "mongoose";
+// import User from "../schemas/User.js"; // your Mongoose model
+// import dotenv from "dotenv";
 
-dotenv.config();
-const uri = process.env.MONGO_URL;
-// Connect to MongoDB
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// dotenv.config();
+// const uri = process.env.MONGO_URL;
+// // Connect to MongoDB
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-// --- Cron job ---
-// Run every 10 minutes between 9:00–20:59, Sunday to Friday
-cron.schedule("*/1 9-20 * * 0-5", async () => {
-  console.log("RUNNING CRON");
+// // --- Cron job ---
+// // Run every 10 minutes between 9:00–20:59, Sunday to Friday
+// cron.schedule("*/1 9-20 * * 0-5", async () => {
+//   console.log("RUNNING CRON");
 
-  try {
-    // Get current Israel local time
-    const now = new Date();
-    const israelTime = now.toLocaleString("en-US", {
-      timeZone: "Asia/Jerusalem",
-    });
-    const currentHour = new Date(israelTime).getHours();
-    const currentDay = new Date(israelTime).getDay(); // 6 = Saturday
+//   try {
+//     // Get current Israel local time
+//     const now = new Date();
+//     const israelTime = now.toLocaleString("en-US", {
+//       timeZone: "Asia/Jerusalem",
+//     });
+//     const currentHour = new Date(israelTime).getHours();
+//     const currentDay = new Date(israelTime).getDay(); // 6 = Saturday
 
-    // Extra safety: skip if Saturday
-    if (currentDay === 6) {
-      console.log("It's Saturday (Shabbat) – skipping reminders.");
-      return;
-    }
+//     // Extra safety: skip if Saturday
+//     if (currentDay === 6) {
+//       console.log("It's Saturday (Shabbat) – skipping reminders.");
+//       return;
+//     }
 
-    const threeHoursAgo = new Date(Date.now() - 3 * 60 * 60 * 1000);
+//     const threeHoursAgo = new Date(Date.now() - 3 * 60 * 60 * 1000);
 
-    // Find users added >3 hours ago who haven't booked
-    const usersToRemind = await User.find({
-      createdAt: { $lte: threeHoursAgo },
-      calendlyBooked: false,
-      reminderSent: false,
-    });
-  } catch (err) {
-    console.error("Cron job error:", err);
-  }
-});
+//     // Find users added >3 hours ago who haven't booked
+//     const usersToRemind = await User.find({
+//       createdAt: { $lte: threeHoursAgo },
+//       calendlyBooked: false,
+//       reminderSent: false,
+//     });
+//   } catch (err) {
+//     console.error("Cron job error:", err);
+//   }
+// });
